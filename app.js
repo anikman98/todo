19a31412d4f4c56ff468030a70ae46f1715f5f5d
anikman <anikman98@gmail.com>
2023-07-19 00:08:03 +0530

    added the signup and login form and added cookie parser

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieParser = require('cookie-parser');
+
+const todoRoutes = require('./routes/todo');
+const indexRoutes = require('./routes/index');
+const authRoutes = require('./routes/auth')
+
 const app = express();
 
 mongoose.connect("mongodb://0.0.0.0:27017/tic_tac_toe", {
@@ -11,13 +17,20 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 app.use(express.json());
+app.use(cookieParser());
 
 app.set("view engine", "ejs");
 
 //routes
-app.use(require("./routes/index"));
-app.use(require("./routes/todo"));
-app.use(require('./routes/auth'));
+app.use(indexRoutes);
+app.use(todoRoutes);
+app.use(authRoutes);
+
+// app.get('/set-cookies', (req, res) => {
+//   res.setHeader('Set-Cookie', 'newUser=true');
+
+//   res.send('you got the cookies');
+// })
 
 app.listen(3000, () => {
   console.log("Server started on port: 3000");
